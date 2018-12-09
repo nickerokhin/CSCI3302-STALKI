f30d8e10d8bad382174708f00c979ed5ea1d4546
ForrestBarnes <forrest.barnes@colorado.edu>
2018-12-09 15:50:36 -0700

    If JS was not trash this would work.

--- src/drone_test.js
+++ src/drone_test.js
@@ -5,15 +5,22 @@ var drone = bebop.createClient();
 try {
   drone.connect(function() {
     drone.takeOff();
-    throw("We got an error mid-flight!");
-    setTimeout(function() {
+
+    console.log(1);
+    setTimeout(function(){
+      console.log(2);
+      // throw("We got an error mid-flight!");
       drone.land();
     }, 5000);
   });
 }
-catch(err)
+catch(ex)
 {
+  console.log(3);
   // Ensures the drone lands if the code breaks
   drone.land();
-  throw(err);
+  console.log(4);
+  setTimeout(function(){
+    callback(ex);
+  }, 5000);
 }
