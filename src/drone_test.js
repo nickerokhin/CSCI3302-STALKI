2dff93acddbb49c5c35354c836fc4591a739c46d
ForrestBarnes <forrest.barnes@colorado.edu>
2018-12-09 14:32:22 -0700

    Modified drone test to automatically land upon error.

--- src/drone_test.js
+++ src/drone_test.js
@@ -2,10 +2,18 @@ var bebop = require('node-bebop');
 
 var drone = bebop.createClient();
 
-drone.connect(function() {
-  drone.takeOff();
-
-  setTimeout(function() {
-    drone.land();
-  }, 5000);
-});
+try {
+  drone.connect(function() {
+    drone.takeOff();
+    throw("We got an error mid-flight!");
+    setTimeout(function() {
+      drone.land();
+    }, 5000);
+  });
+}
+catch(err)
+{
+  // Ensures the drone lands if the code breaks
+  drone.land();
+  throw(err);
+}
