599bd6c37b0d4e627e91c011e7746b77d33afbe9
Carl Cortright <carl.cortright@coinbase.com>
2018-12-14 15:22:02 -0800

    remove old node work

--- python/main.py
+++ python/main.py
@@ -8,6 +8,9 @@ socketio = SocketIO(app)
 @socketio.on('my event')
 def handle_message(message):
     print('received message: ' + str(message))
+    send("hello world")
 
 if __name__ == '__main__':
+    print("before")
     socketio.run(app)
+    print("after")
