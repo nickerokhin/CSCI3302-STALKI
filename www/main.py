0e17f83c94ce617d477261f4217d48bdd540a3eb
Blaise Page <blpa1200@colorado.edu>
2018-12-14 17:46:34 -0700

    added web app code

---
+++ www/main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+# initialize Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    """Serve the index HTML"""
+    return render_template('index.html')
