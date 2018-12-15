cfd25e8c3b20c56c1f9281b2ec57a6415cfcd7ab
Nick Erokhin <nickerokhin@gmail.com>
2018-12-14 17:29:19 -0700

    ..

--- python/flyDrone.py
+++ python/flyDrone.py
@@ -59,169 +59,171 @@ class UserVision:
             #cv2.imwrite(filename, img)
             self.index +=1
 
-
-# make my bebop object
-bebop = Bebop()
-queue = Queue()
-# connect to the bebop
-p = Process(target=write_image, args=(queue,))
-p.start()
-success = bebop.connect(5)
-clear_images()
-if (success):
-    # start up the video
-    bebopVision = DroneVision(bebop, is_bebop=True)
-
-    userVision = UserVision(bebopVision)
-    bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
-    success = bebopVision.open_video()
-
+def start_flight():
+    # make my bebop object
+    bebop = Bebop()
+    queue = Queue()
+    # connect to the bebop
+    p = Process(target=write_image, args=(queue,))
+    p.start()
+    success = bebop.connect(5)
+    clear_images()
     if (success):
-        print("Vision successfully started!")
-        #removed the user call to this function (it now happens in open_video())
-        #bebopVision.start_video_buffering()
+        # start up the video
+        bebopVision = DroneVision(bebop, is_bebop=True)
 
-        # skipping actually flying for safety purposes indoors - if you want
-        # different pictures, move the bebop around by hand
-        print("Fly me around by hand!")
-        #bebop.smart_sleep(5)
+        userVision = UserVision(bebopVision)
+        bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
+        success = bebopVision.open_video()
 
-        then = datetime.datetime.now()
-        now = datetime.datetime.now()
-        bebop.safe_takeoff(10)
-        inc = 0
-        while (now - then).total_seconds() < 20:
-            try:
-                im = bebopVision.get_latest_valid_picture()
-                hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-                dimensions = im.shape
-                #print(dimensions)
-                height = dimensions[0]
-                width = dimensions[1]
-                left_bound = width/3
-                right_bound = (width/3) * 2
-                upper_bound = height/5
-                lower_bound = (height/5) * 4
-                #upper_bound -= 50
-                #lower_bound -= 50
-                #Green color in HSV
-                #low = np.array([40, 50, 50])
-                #high = np.array([80, 255, 255])
-                #Pink color in GBR
-                #low = np.array([72, 33, 195])
-                #high = np.array([203, 192, 255])
-                #Blue color in GBR
-                low = np.array([100, 50, 50])
-                high = np.array([140, 255, 255])
-                image_mask = cv2.inRange(hsv, low, high)
-                kern = np.ones((9,9), np.uint8)
-                mask = cv2.morphologyEx(image_mask, cv2.MORPH_OPEN, kern)
-                mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kern)
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-                biggestRect = None
-                biggestRectArea = 0
-                for c in cnts:
-                    rect = cv2.boundingRect(c)
-                    x,y,w,h = rect
-                    area = w*h
-                    if area > biggestRectArea:
-                        biggestRect = rect
-                        biggestRectArea = area
-                if biggestRect is not None:
-                    x, y, w, h = biggestRect
-                    #print("Rectangle area", biggestRectArea)
-                    #Area range, 9000-13000
-                    #Below 9000, move forward
-                    #Above 13000, move backward
+        if (success):
+            print("Vision successfully started!")
+            #removed the user call to this function (it now happens in open_video())
+            #bebopVision.start_video_buffering()
 
-                    centerX = int(x + w/2)
-                    centerY = int(y + h/2)
-                    queue.put((im, biggestRect, inc, (centerX, centerY), (upper_bound, lower_bound)))
-                    #print("Center X:", centerX)
-                    #print("Center Y:", centerY)
+            # skipping actually flying for safety purposes indoors - if you want
+            # different pictures, move the bebop around by hand
+            print("Fly me around by hand!")
+            #bebop.smart_sleep(5)
+
+            then = datetime.datetime.now()
+            now = datetime.datetime.now()
+            bebop.safe_takeoff(10)
+            inc = 0
+            while (now - then).total_seconds() < 20:
+                try:
+                    im = bebopVision.get_latest_valid_picture()
+                    hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+                    dimensions = im.shape
+                    #print(dimensions)
+                    height = dimensions[0]
+                    width = dimensions[1]
+                    left_bound = width/3
+                    right_bound = (width/3) * 2
+                    upper_bound = height/5
+                    lower_bound = (height/5) * 4
+                    #upper_bound -= 50
+                    #lower_bound -= 50
+                    #Green color in HSV
+                    #low = np.array([40, 50, 50])
+                    #high = np.array([80, 255, 255])
+                    #Pink color in GBR
+                    #low = np.array([72, 33, 195])
+                    #high = np.array([203, 192, 255])
+                    #Blue color in GBR
+                    low = np.array([100, 50, 50])
+                    high = np.array([140, 255, 255])
+                    image_mask = cv2.inRange(hsv, low, high)
+                    kern = np.ones((9,9), np.uint8)
+                    mask = cv2.morphologyEx(image_mask, cv2.MORPH_OPEN, kern)
+                    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kern)
+                    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+                    biggestRect = None
+                    biggestRectArea = 0
+                    for c in cnts:
+                        rect = cv2.boundingRect(c)
+                        x,y,w,h = rect
+                        area = w*h
+                        if area > biggestRectArea:
+                            biggestRect = rect
+                            biggestRectArea = area
+                    if biggestRect is not None:
+                        x, y, w, h = biggestRect
+                        #print("Rectangle area", biggestRectArea)
+                        #Area range, 9000-13000
+                        #Below 9000, move forward
+                        #Above 13000, move backward
+
+                        centerX = int(x + w/2)
+                        centerY = int(y + h/2)
+                        queue.put((im, biggestRect, inc, (centerX, centerY), (upper_bound, lower_bound)))
+                        #print("Center X:", centerX)
+                        #print("Center Y:", centerY)
+                        
+                        #if biggestRect is not None:
+                        #print("here")
+                        
+                        if centerY < upper_bound:
+                            move_up(bebop, 20, 0.5)
+                            print("Moving up")
+                            time.sleep(0.5)
+                            inc += 1
+
+
+                        elif centerY > lower_bound:
+                            move_down(bebop, 20, 0.5)
+                            print("Moving down")
+                            time.sleep(0.5)
+                            inc += 1
+
+
+                        elif biggestRectArea < 8000 and centerX < left_bound:
+                            move_right(bebop, 20, 1)
+                            print("Moving right")
+                            time.sleep(1)
+                            inc += 1
+
+
+                        elif biggestRectArea > 8000 and centerX > right_bound:
+                            move_left(bebop, 20, 1)
+                            print("Moving left")
+                            time.sleep(1)
+                            inc += 1
+
+
+                        elif biggestRectArea < 8000:
+                            print("Moving forward")
+                            move_forward(bebop, 20, 1)
+                            time.sleep(1)
+                            inc += 1
+
+
+                        elif biggestRectArea > 15000:
+                            move_backward(bebop, 20, 1)
+                            print("Moving backward")
+                            #move_backward(bebop, 20, 2)
+                            time.sleep(1)
+                            inc += 1
+
+
+                        elif centerX < left_bound:
+                            move_right(bebop, 20, 1)
+                            print("Moving right")
+                            time.sleep(1)
+                            inc += 1
+
+
+                        elif centerX > right_bound:
+                            move_left(bebop, 20, 1)
+                            print("Moving left")
+                            time.sleep(1)
+                            inc += 1
+
                     
-                    #if biggestRect is not None:
-                    #print("here")
-                    
-                    if centerY < upper_bound:
-                        move_up(bebop, 20, 0.5)
-                        print("Moving up")
-                        time.sleep(0.5)
-                        inc += 1
 
 
-                    elif centerY > lower_bound:
-                        move_down(bebop, 20, 0.5)
-                        print("Moving down")
-                        time.sleep(0.5)
-                        inc += 1
+                    now = datetime.datetime.now()
+                except KeyboardInterrupt:
+                    print("Emergency landing...")
+                    bebop.emergency_land()
+                    exit()
+                except Exception as e:
+                    print(e)
+                    bebop.emergency_land()
+                    exit()
+            print("Moving the camera using velocity")
+            #bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=4)
+            #bebop.smart_sleep(25)
+            print("Finishing demo and stopping vision")
+            #bebopVision.close_video()
 
 
-                    elif biggestRectArea < 8000 and centerX < left_bound:
-                        move_right(bebop, 20, 1)
-                        print("Moving right")
-                        time.sleep(1)
-                        inc += 1
+        # disconnect nicely so we don't need a reboot
+        print("Finishing demo, landing....")
+        bebop.safe_land(20)
+        #bebop.disconnect()
+    else:
+        print("Error connecting to bebop.  Retry")
 
-
-                    elif biggestRectArea > 8000 and centerX > right_bound:
-                        move_left(bebop, 20, 1)
-                        print("Moving left")
-                        time.sleep(1)
-                        inc += 1
-
-
-                    elif biggestRectArea < 8000:
-                        print("Moving forward")
-                        move_forward(bebop, 20, 1)
-                        time.sleep(1)
-                        inc += 1
-
-
-                    elif biggestRectArea > 15000:
-                        move_backward(bebop, 20, 1)
-                        print("Moving backward")
-                        #move_backward(bebop, 20, 2)
-                        time.sleep(1)
-                        inc += 1
-
-
-                    elif centerX < left_bound:
-                        move_right(bebop, 20, 1)
-                        print("Moving right")
-                        time.sleep(1)
-                        inc += 1
-
-
-                    elif centerX > right_bound:
-                        move_left(bebop, 20, 1)
-                        print("Moving left")
-                        time.sleep(1)
-                        inc += 1
-
-                
-
-
-                now = datetime.datetime.now()
-            except KeyboardInterrupt:
-                print("Emergency landing...")
-                bebop.emergency_land()
-                exit()
-            except Exception as e:
-                print(e)
-                bebop.emergency_land()
-                exit()
-        print("Moving the camera using velocity")
-        #bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=4)
-        #bebop.smart_sleep(25)
-        print("Finishing demo and stopping vision")
-        #bebopVision.close_video()
-
-
-    # disconnect nicely so we don't need a reboot
-    print("Finishing demo, landing....")
-    bebop.safe_land(20)
-    #bebop.disconnect()
-else:
-    print("Error connecting to bebop.  Retry")
-
+if __name__ == "__main__":
+    start_flight()
