1004877ffa2b9d2a3e9e0210d62cf6d0edb73d3d
Nick Erokhin <nickerokhin@gmail.com>
2018-12-14 18:40:33 -0700

    dummy flight code

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ sys.path.append('./web/')
 from multiprocessing import Process, Queue
 from flight.fly_drone import *
 from www.server import *
+import time
+
+dir_list = ["left", "right", "up", "down", "left", "right", "forward", "backward"]
+
+
+def demo_flight(q):
+	for i in dir_list:
+		q.put(i)
+		time.sleep(1)
+
+
+
+
 
 if __name__ == '__main__':
-    q = Queue()
+	q = Queue()
 	web = Process(target=run_server, args=(q,))
-	drone = Process(target=start_flight, args=(q,))
-    web.start()
+	drone = Process(target=demo_flight, args=(q,))
+	web.start()
 	drone.start()
+	drone.join()
