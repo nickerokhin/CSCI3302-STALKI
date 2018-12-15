81cbd83682bbd676eb2cda5e9763515ea4f112da
Nick Erokhin <nickerokhin@gmail.com>
2018-12-14 19:16:45 -0700

    Hacky solution for flask push

--- www/server.py
+++ www/server.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, emit
-socketio = SocketIO(app)
+from threading import Lock
+
 
 # initialize Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    """Serve the index HTML"""
-    return render_template('index.html')
 
 
 def run_server(q):
-	app.run()
-	while True:
-		movement = q.get()
-		emit('move', {'move': movement})
+	
+	app = Flask(__name__)
+	socketio = SocketIO(app, async_mode='threading')
+	thread = None
+	thread_lock = Lock()
+	@app.route('/')
+	def index():
+		"""Serve the index HTML"""
+		return render_template('index.html')
+
+
+	@socketio.on('direction')
+	def receive_connections(res):
+		print("BEFORE LOOP")
+		while True:
+			movement = q.get()
+			print(movement)
+			
+			emit('move', {'move': movement})
+	socketio.run(app, debug=True)
+	
